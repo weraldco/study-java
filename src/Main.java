29911f2e6ea8720e1c7a33c0660aeeeb0ff222a7
Werald Opolento <werald.opolento@gmail.com>
2025-06-19 20:36:51 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner  scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter you age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old!");
+    }
+}
