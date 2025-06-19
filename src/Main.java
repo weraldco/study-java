d357b57bb8e176c08866445b531b031f89883b56
Werald Opolento <werald.opolento@gmail.com>
2025-06-19 20:48:35 +0800

    first day of Java study learn about variable and printing and input user

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner  scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter your name: ");
+//        String name = scanner.nextLine();
+//
+//        System.out.print("Enter you age: ");
+//        int age = scanner.nextInt();
+//
+//        System.out.println("Hello " + name);
+//        System.out.println("You are " + age + " years old!");
 
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+//       Calculate the area of a rectagle
+//        Formula : A = L x W;
+        System.out.println("Getting the Area of  RECTANGLE");
+        System.out.println("Width: ");
+        double width = scanner.nextDouble();
 
-        System.out.print("Enter you age: ");
-        int age = scanner.nextInt();
+        System.out.println("Length: ");
+        double length = scanner.nextDouble();
 
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old!");
+        double area = width * length;
+
+        System.out.println("AREA: " + area + " cm²");
+        scanner.close();
     }
 }
